98c9ab85ee0a0963763be1a6e0fe19f4094a9a15
olixc <noibiabdulrasaq@rocketmail.com>
2023-11-13 12:44:03 +0100

    Modify shell.h, main.c and string_format.c file

--- shell.h
+++ shell.h
@@ -10,8 +10,10 @@
 
 #define MAX_INPUT_LEN 1024
 
+char *_strdup(const char *str);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 char *_strtrim(char *str);
-void execute_command(char *command);
 
 #endif
